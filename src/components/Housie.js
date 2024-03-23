87ab31082b91523abd2ec94e2e0bc1ac40f5ba28
pramodyadav <pramodryadavall@gmail.com>
2024-03-23 23:43:31 +0530

    removed unneccessary files

--- src/components/Housie.js
+++ src/components/Housie.js
@@ -1,48 +1,49 @@
 import React, { useState } from 'react'
 
 function Housie() {
-  const [arr] = useState([0,1, 2, 3, 4, 5, 6, 7, 8, 9]);
-const [selectedBtn,setSelectedBtn] = useState([]);
-const handleClick=()=>{
-  if(arr.length>0){
-    for(let i=0;i<arr.length;i++){
-      let j=Math.floor(Math.random()*i);
-      let swap=arr[i];
-      arr[i]=arr[j];
-      arr[j]=swap;
+  const [arr] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const [selectedBtn, setSelectedBtn] = useState([]);
+  const handleClick = () => {
+    if (arr.length > 0) {
+      for (let i = 0; i < arr.length; i++) {
+        let j = Math.floor(Math.random() * i);
+        let swap = arr[i];
+        arr[i] = arr[j];
+        arr[j] = swap;
+      }
+      setSelectedBtn([...selectedBtn, arr.pop()]);
+
+
+      console.log(selectedBtn);
+      console.log(arr);
     }
-    setSelectedBtn([...selectedBtn,arr.pop()]);
-   
-  
-  console.log(selectedBtn);
-  console.log(arr);
-  }
     else {
       alert("Please stop! there is no circle left to color. Please refresh the page and try again");
     }
 
 
 
-}
+  }
 
   return (
     <>
 
       <div class="container">
-        <div class="row">
-          {[...Array(10).keys()].map((i)=><div style={{width:'25px',borderRadius:'50%', textAlign:'center', float:'right', backgroundColor:selectedBtn.includes(i)?'red' :'white'}}>{i}</div>)}
+        <div className='circleContainer'>
+          {[...Array(10).keys()].map((i) =>
+            <div className='cirle' style={{ backgroundColor: selectedBtn.includes(i) ? 'red' : 'white' }}>{i}</div>
+          )}
           
         </div>
+        <div style={{ textAlign: 'center' }}>
+            <button className='Btn' onClick={handleClick}>Generate</button>
+          </div>
 
-
-        <div class="row">
-          <button id="gen" onClick={handleClick}>Generate</button>
-        </div>
       </div>
 
 
     </>
-    
+
   )
 }
 
